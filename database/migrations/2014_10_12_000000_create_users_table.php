28906b72701fc4e56c5ec431e20fe585aa6e2c6c
travislykes <fopokuamoako@gmail.com>
2019-11-21 18:56:24 +0100

    admin assets

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id');
             $table->primary('id');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
